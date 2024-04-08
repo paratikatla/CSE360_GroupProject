ce0d3679781182b415290ce6394e3ef2c1051d6d
Rose Geisler <rosiegeisler@gmail.com>
2024-04-07 20:50:45 -0700

    modiefied: PatientAppointmentHistory

--- Appointment.java
+++ Appointment.java
@@ -1,6 +1,73 @@
 package CSE360_GroupProject;
+
+
 public class Appointment {
-    //private Doctor doctor;
-    //private Nurse nurse;
-    //
+    private Doctor doctor;
+    private Nurse nurse;
+    private Patient patient;
+    private String date;
+    private String reason;
+    private NurseExam nurseExam;
+    private DoctorExam docExam;
+    public Doctor getDoctor() {
+        return doctor;
+    }
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+    public Nurse getNurse() {
+        return nurse;
+    }
+    public void setNurse(Nurse nurse) {
+        this.nurse = nurse;
+    }
+    public Patient getPatient() {
+        return patient;
+    }
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public String getReason() {
+        return reason;
+    }
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+    public NurseExam getNurseExam() {
+        return nurseExam;
+    }
+    public void setNurseExam(NurseExam nurseExam) {
+        this.nurseExam = nurseExam;
+    }
+    public DoctorExam getDocExam() {
+        return docExam;
+    }
+    public void setDocExam(DoctorExam docExam) {
+        this.docExam = docExam;
+    }
+    public Appointment(Patient patient, String date, String reason) {
+        this.patient = patient;
+        this.date = date;
+        this.reason = reason;
+    }
+    public Appointment(Doctor doctor, Nurse nurse, Patient patient, String date, String reason, NurseExam nurseExam,
+            DoctorExam docExam) {
+        this.doctor = doctor;
+        this.nurse = nurse;
+        this.patient = patient;
+        this.date = date;
+        this.reason = reason;
+        this.nurseExam = nurseExam;
+        this.docExam = docExam;
+    }
+
+    public Appointment(String fileName){
+
+    }
 }
--- NurseExam.java
+++ NurseExam.java
@@ -1,4 +1,72 @@
 package CSE360_GroupProject;
 public class NurseExam {
+    private String allergies;
+    private String healthConcerns;
+    private Double weight;
+    private Double height;
+    private Double bodyTemp;
+    private Double bloodPressure;
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+    public String getHealthConcerns() {
+        return healthConcerns;
+    }
+    public void setHealthConcerns(String healthConcerns) {
+        this.healthConcerns = healthConcerns;
+    }
+    public Double getWeight() {
+        return weight;
+    }
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+    public Double getHeight() {
+        return height;
+    }
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+    public Double getBodyTemp() {
+        return bodyTemp;
+    }
+    public void setBodyTemp(Double bodyTemp) {
+        this.bodyTemp = bodyTemp;
+    }
+    public Double getBloodPressure() {
+        return bloodPressure;
+    }
+    public void setBloodPressure(Double bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
     
+    public NurseExam(String allergies, String healthConcerns, Double weight, Double height, Double bodyTemp,
+            Double bloodPressure) {
+        this.allergies = allergies;
+        this.healthConcerns = healthConcerns;
+        this.weight = weight;
+        this.height = height;
+        this.bodyTemp = bodyTemp;
+        this.bloodPressure = bloodPressure;
+    }
+
+    public NurseExam(String allergies, String healthConcerns) {
+        this.allergies = allergies;
+        this.healthConcerns = healthConcerns;
+    }
+
+    @Override
+    public String toString() {
+        return "NurseExam [allergies=" + allergies + ", healthConcerns=" + healthConcerns + ", weight=" + weight
+                + ", height=" + height + ", bodyTemp=" + bodyTemp + ", bloodPressure=" + bloodPressure + "]";
+    }
+
+    public String toStringUnderTwelveString (){
+        return "NurseExamUnder12 [allergies=" + allergies + ", healthConcerns=" + healthConcerns +  "]";
+    }
 }
