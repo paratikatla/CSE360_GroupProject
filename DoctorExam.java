007ed032fa16f6ecc9e0ce6314ee733e07535370
Rose Geisler <rosiegeisler@gmail.com>
2024-04-07 20:52:00 -0700

    modiefied PatientAppointmentHistory

--- DoctorExam.java
+++ DoctorExam.java
@@ -1,4 +1,42 @@
 package CSE360_GroupProject;
 public class DoctorExam {
+    private String notes;
+    private String prescription;
+    private String prescriptionQuantity;
     
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getPrescription() {
+        return prescription;
+    }
+
+    public void setPrescription(String prescription) {
+        this.prescription = prescription;
+    }
+
+    public String getPrescriptionQuantity() {
+        return prescriptionQuantity;
+    }
+
+    public void setPrescriptionQuantity(String prescriptionQuantity) {
+        this.prescriptionQuantity = prescriptionQuantity;
+    }
+
+    public DoctorExam(String notes, String prescription, String prescriptionQuantity) {
+        this.notes = notes;
+        this.prescription = prescription;
+        this.prescriptionQuantity = prescriptionQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return "DoctorExam [notes=" + notes + ", prescription=" + prescription + ", prescriptionQuantity="
+                + prescriptionQuantity + "]";
+    }
 }
