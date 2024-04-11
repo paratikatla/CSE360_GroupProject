e973e1bb71265b6bdf4a963dda5a660f4103deba
Chaitanyasulam <158023811+Chaitanyasulam@users.noreply.github.com>
2024-04-10 21:51:31 -0700

    Update patientaccountsettingspage2.java

--- FrontEnd/patientaccountsettingspage2.java
+++ FrontEnd/patientaccountsettingspage2.java
@@ -2,16 +2,7 @@
 
 
 
-  VBox mainLayout = new VBox(30,header,headerBox, totalbox, changePasswordButton, buttonsBox);
-        mainLayout.setPadding(new Insets(0));
-        mainLayout.setAlignment(Pos.CENTER.TOP_CENTER);
-
-        // Scene and Stage setup
-        Scene scene = new Scene(mainLayout, 900, 600);
-        stage.setScene(scene);
-        stag
- ER.TOP_CENTER);
-
+ 
         // Scene and Stage setup
         Scene scene = new Scene(mainLayout, 900, 600);
         stage.setScene(scene);
