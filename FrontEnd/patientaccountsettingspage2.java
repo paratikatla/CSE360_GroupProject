6debe95a0841e8d7f206d006dfb45d4acd3c6c63
Hemanth2334 <165976410+Hemanth2334@users.noreply.github.com>
2024-04-10 22:02:11 -0700

    Update patientaccountsettingspage2.java

--- FrontEnd/patientaccountsettingspage2.java
+++ FrontEnd/patientaccountsettingspage2.java
@@ -229,10 +229,8 @@ VBox mainLayout = new VBox(30,header,headerBox, totalbox, changePasswordButton, 
         mainLayout.setAlignment(Pos.CENTER.TOP_CENTER);
 
         // Scene and Stage setup
-        Scene scene = new Scene(mainLayout, 900, 600);
-        stage.setScene(scene);
-        stage.show();
-    }
+ 
+
 
         // Scene and Stage setup
         Scene scene = new Scene(mainLayout, 900, 600);
