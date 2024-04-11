cb7ea93c7116343063bfbc544f034fac5f1e6184
Chaitanyasulam <158023811+Chaitanyasulam@users.noreply.github.com>
2024-04-10 21:52:47 -0700

    Update patientaccountsettingspage2.java

--- FrontEnd/patientaccountsettingspage2.java
+++ FrontEnd/patientaccountsettingspage2.java
@@ -15,12 +15,13 @@
         stage.show();
     }
 
-  VBox mainLayout = new VBox(30,header,headerBox, totalbox, changePasswordButton, buttonsBox);
-        mainLayout.setPadding(new Insets(0));
-        mainLayout.setAlignment(Pos.CENTER.TOP_CENTER);
-
         // Scene and Stage setup
         Scene scene = new Scene(mainLayout, 900, 600);
         stage.setScene(scene);
         stage.show();
     }
+
+
+  VBox mainLayout = new VBox(30,header,headerBox, totalbox, changePasswordButton, buttonsBox);
+        mainLayout.setPadding(new Insets(0));
+        mainLayout.setAlignment(Pos.CENTER.TOP_CENTER);
